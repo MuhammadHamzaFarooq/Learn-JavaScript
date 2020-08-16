77a54259a19a7e2d1c13aa26a2b1d1125f0773d0
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2020-08-16 09:04:15 -0700

    chapter 1

---
+++ Practices JavaScript/tut1.js
@@ -0,0 +1,35 @@
+//Day  One  16/8/202
+
+
+//        ========================= Chapter1 (ALERTS) ================= 
+
+//Q1- 1. Write a script to greet your website visitor using JS alert box.
+alert("Assalam O Aliakum");
+
+//Q2 - 2. Write a script to display following message on your web page:
+alert("Please enter valid password");
+
+//Q3- 3. Write a script to display following message on your web page: (Hint : Use line break)
+alert("Welcome to JS Land .... \n Happy Coding");
+
+//Q4 - 4. Write a script to display following messages in sequence:
+// a
+alert("Welcome to JS Land");
+// b
+alert("Happy Coding \n Prevent this page additional dialogs");
+
+//Q4 - 5. Generate the following message through browser’s developer console:
+alert("Hello.. I can run JS through my web browser's console ");
+
+//Q6 - 6. Make use of alerts in your new/existing HTML & CSS project
+
+// Q7 - 7. Practice placement of <script></script> element in following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
+
+//        ========================= Chapter2 (VARIABLES FOR STRINGS ) ================= 
+
+//Q1 -
